897eeba5c97d669837f6d2a2eb390673839d3229
David Salgado <ballesto_@hotmail.com>
2021-09-10 23:41:15 -0500

    Update schema.sql

--- deploy/schema.sql
+++ deploy/schema.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE IF NOT EXISTS fonyou;
 USE fonyou;
-CREATE TABLE employees (
+CREATE TABLE IF NOT EXISTS employees (
      id                      INT NOT NULL AUTO_INCREMENT,
      first_name              VARCHAR(255) NOT NULL,
      last_name               VARCHAR(255) NOT NULL,
-     start_date     	     DATE NOT NULL COMMENT 'Date of joining the company.',
-     end_date		     DATE COMMENT 'Date of departure from the company.',
+     start_date     	    DATE NOT NULL COMMENT 'Date of joining the company.',
+     end_date		         DATE COMMENT 'Date of departure from the company.',
      base_salary             NUMERIC(19, 2) NOT NULL,
      PRIMARY KEY (id)
 );
