598dfa8714d1580c2f0a46a6c11b89d7bfdd6f91
davidpdp42 <ballesteros.johan.david@gmail.com>
2021-09-11 08:37:15 -0500

    hot fix restrictions

--- core/src/test/java/com/fonyou/CoreApplicationTests.java
+++ core/src/test/java/com/fonyou/CoreApplicationTests.java
@@ -1,48 +1,69 @@
-//package com.fonyou;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.util.MimeTypeUtils;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fonyou.entities.Employee;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//class CoreApplicationTests {
-//
-//	private MockMvc mockMvc;
-//	
-//	public CoreApplicationTests(MockMvc mockMvc) {
-//		this.mockMvc = mockMvc;
-//	}
-//	
-//	private ObjectMapper mapper = new ObjectMapper();
-//	
-//	@Test
-//	public void should_No_Save_Employee_When_Mandatory_Field_Is_Empty() throws Exception {
-//		
-//		Employee newEmployee = Employee.buildFromMandatoryFields(
-//				"John", "Doe", null, null);
-//		
-//		mockMvc.perform(post("/")
-//			.contentType(MimeTypeUtils.APPLICATION_JSON_VALUE)
-//			.content(mapper.writeValueAsString(newEmployee))
-//			.accept(MimeTypeUtils.APPLICATION_JSON_VALUE)
-//		).andExpect(status().is(HttpStatus.OK.value()))
-//         .andExpect(header().string("Location", "/api/account/12345"))
-//         .andExpect(jsonPath("$.").value("12345")) 
-//         .andExpect(jsonPath("$.accountType").value("SAVINGS"))
-//         .andExpect(jsonPath("$.balance").value(5000));
-//		
-//	}
-//
-//}
+package com.fonyou;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.MimeTypeUtils;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fonyou.entities.Employee;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CoreApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	private static ObjectMapper mapper = new ObjectMapper();
+	
+	@BeforeAll
+	static void setUp() {
+		// Config to new mapper instance can serialize/deserialize dates.
+		mapper.registerModule(new JavaTimeModule());
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		mapper.setSerializationInclusion(Include.NON_NULL);
+	}
+	
+	@Test
+	public void should_No_Save_Employee_When_Mandatory_Field_Is_Null() throws Exception {
+		
+		// Given
+		var nullEmployees = List.of(
+			Employee.buildFromMandatoryFields(null, "test", LocalDate.now(), BigDecimal.ONE),
+			Employee.buildFromMandatoryFields("test", null, LocalDate.now(), BigDecimal.ONE),
+			Employee.buildFromMandatoryFields("test", "test", null, BigDecimal.ONE),
+			Employee.buildFromMandatoryFields("test", "test", LocalDate.now(), null)
+		);
+		
+		for (Employee nullEmployee : nullEmployees) {
+			
+			// When
+			var result = mockMvc.perform(post("/employees")
+				.contentType(MimeTypeUtils.APPLICATION_JSON_VALUE)
+				.content(mapper.writeValueAsString(nullEmployee))
+				.accept(MimeTypeUtils.APPLICATION_JSON_VALUE)
+			);
+							
+			// Then
+			result.andExpect(status().isOk())
+				  .andExpect(jsonPath("$.error").value("no required field can be null."));
+		}
+		
+	}
+		
+}
